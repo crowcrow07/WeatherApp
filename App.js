ec050885586bad2a1ea95362616676ad07fcbccf
crowcrow07 <skathd3402@gmail.com>
2023-11-20 17:05:27 +0900

    refactor : 의존성 낮추기, 함수형 프로그래밍

--- App.js
+++ App.js
@@ -2,43 +2,66 @@ import * as Location from "expo-location";
 import { useEffect, useState } from "react";
 import { Text, View, ScrollView } from "react-native";
 
-import { API_KEY } from "@env";
+import WeatherInfo from "./components/WeatherInfo";
 
+import { API_KEY } from "@env";
 import { styles } from "./style";
 
-export default function App() {
+const App = () => {
   const [city, setCity] = useState("Loading...");
   const [days, setDays] = useState([{ temp: 0, weather: "sunny" }]);
   const [ok, setOk] = useState(true);
-  const getWeather = async () => {
+
+  const getLocation = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
       setOk(false);
     }
+
     const {
       coords: { latitude, longitude },
     } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+
+    return { latitude, longitude };
+  };
+
+  const reverseGeocode = async (latitude, longitude) => {
     const location = await Location.reverseGeocodeAsync(
       { latitude, longitude },
       { useGoogleMaps: false }
     );
-    setCity(location[0].region);
+    return location[0].region;
+  };
+
+  const fetchWeather = async (latitude, longitude) => {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
     );
     const json = await response.json();
 
-    setDays([
-      {
-        ...days,
-        temp: Math.round(json.main.temp - 273.15),
-        weather: json.weather[0].main,
-      },
-    ]);
+    return {
+      temp: Math.round(json.main.temp - 273.15),
+      weather: json.weather[0].main,
+    };
   };
+
+  const getWeather = async () => {
+    try {
+      const { latitude, longitude } = await getLocation();
+      const city = await reverseGeocode(latitude, longitude);
+      const weatherInfo = await fetchWeather(latitude, longitude);
+
+      setCity(city);
+      setDays([weatherInfo]);
+    } catch (error) {
+      console.error("Error getting weather:", error);
+    }
+  };
+
   useEffect(() => {
     getWeather();
   }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
@@ -50,11 +73,10 @@ export default function App() {
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>{days[0].temp}</Text>
-          <Text style={styles.description}>{days[0].weather}</Text>
-        </View>
+        <WeatherInfo temp={days[0].temp} weather={days[0].weather} />
       </ScrollView>
     </View>
   );
-}
+};
+
+export default App;
